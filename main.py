9416727a317ba5f926ed91373077a59222eb892c
Alfred Myrne <alfredmyrneblader@gmail.com>
2021-04-04 16:02:47 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import urllib.request, json
+import pandas as pd
+
+
+def load_json():
+    results = []
+    next_url = "https://catalog.forsakringskassan.se/rowstore/dataset/6733a920-64e4-44ac-b0a4-63138ab2c2cf/json"
+
+    i = 0
+    while next_url:
+        i += 1
+        print(i)
+        with urllib.request.urlopen(next_url) as url:
+            json_data_part = json.loads(url.read().decode())
+
+        next_url = json_data_part['next'] if json_data_part['next'] else ""
+
+        results += json_data_part['results']
+
+
+
+        # Loopa genom results lägga till en rad per objekt i dataframen
+
+        # temp = pd.read_json()
+
+        # pd.concat(data, temp)
+
+    return pd.DataFrame(results)
+
+
+def load_csv():
+    # thing
+    data = pd.DataFrame
+    # ladda in fil
+    # gör till dataframe
+
+    pd.read_csv()
+
+
+if __name__ == '__main__':
+    json_data = load_json()
+    print(json_data)
