f28ff686e136e5a149d0dbcada058472a74e24cb
Alfred Myrne <alfredmyrneblader@gmail.com>
2021-04-08 15:26:59 +0200

    added some pandas features

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
-import urllib.request, json
+import json
+
+import numpy as np
 import pandas as pd
+import time
 
 
 def load_json():
-    results = []
-    next_url = "https://catalog.forsakringskassan.se/rowstore/dataset/6733a920-64e4-44ac-b0a4-63138ab2c2cf/json"
-
-    i = 0
-    while next_url:
-        i += 1
-        print(i)
-        with urllib.request.urlopen(next_url) as url:
-            json_data_part = json.loads(url.read().decode())
-
-        next_url = json_data_part['next'] if json_data_part['next'] else ""
-
-        results += json_data_part['results']
-
-        # Loopa genom results lägga till en rad per objekt i dataframen
-
-        # temp = pd.read_json()
-
-        # pd.concat(data, temp)
-
-    return pd.DataFrame(results)
+    return pd.read_json("data/data.json", encoding='unicode_escape')
 
 
 def load_json2():
@@ -35,7 +18,26 @@ def load_csv():
     return pd.read_csv("data/data.csv", encoding='unicode_escape')
 
 
+def handle_data():
+    # remove:
+    # index, kvartal, diagnoskapitel, etc
+    # separera:
+
+    pass
+
+
 if __name__ == '__main__':
-    #json_data = load_json2()
-    csv_data = load_csv()
-    print(csv_data)
+
+    thing = bool(input())
+    if thing:
+        data = load_json()
+    else:
+        data = load_csv()
+
+    data['antal'] = pd.to_numeric(data['antal'], errors='coerce')
+
+    data['andel_man'] = pd.to_numeric(data['andel_man'], errors='coerce')
+    data['andel_kvinnor'] = pd.to_numeric(data['andel_kvinnor'], errors='coerce')
+
+    rd = data.pivot_table(index=["ar","diagnoskapitel_text"], aggfunc={"antal": np.sum, 'andel_man':np.mean, 'andel_kvinnor':np.mean})
+    print(rd)
