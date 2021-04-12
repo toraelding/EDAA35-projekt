ef9af1c19c0afd3cb45b54335788a80115ed150b
Sibbasson <sibbasson@hotmail.com>
2021-04-12 14:12:42 +0200

    r script can now read from json file

--- csv.r
+++ csv.r
@@ -1,6 +1,14 @@
-data <- read.csv("data\\data.csv")
+read_json <- function(file = "data\\data.json") {
+    jsonlite::fromJSON(file)
+}
 
-table <- tapply(as.numeric(data$antal_man), list(data$ar,data$diagnoskapitel_text), sum)
+read_csv <- function(file = "data\\data.csv") {
+    data.frame(read.csv(file))
+}
+
+# table <- tapply(as.numeric(data$antal_man), list(data$ar,data$diagnoskapitel_text), sum)
+
+# print(head(table))
 
 #diagnoskapitel_kod <- data[[3]]
 
